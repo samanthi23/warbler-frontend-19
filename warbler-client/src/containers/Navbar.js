7660dd85abb9c43c9e39021bb3cd8196b7e0e8c9
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-03-27 14:25:53 +0000

    Initial commit

--- warbler-client/src/containers/Navbar.js
+++ warbler-client/src/containers/Navbar.js
@@ -5,43 +5,20 @@ import { logout } from "../store/actions/auth";
 import Logo from "../images/warbler-logo.png";
 
 class Navbar extends Component {
-  logout = e => {
-    e.preventDefault();
-    this.props.logout();
-  };
   render() {
     return (
       <nav className="navbar navbar-expand">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <Link to="/" className="navbar-brand">
-              <img src={Logo} alt="Warbler Home" />
-            </Link>
-          </div>
-          {this.props.currentUser.isAuthenticated ? (
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <Link
-                  to={`/users/${this.props.currentUser.user.id}/messages/new`}
-                >
-                  New Message
-                </Link>
-              </li>
-              <li>
-                <a onClick={this.logout}>Log out</a>
-              </li>
-            </ul>
-          ) : (
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <Link to="/signup">Sign up</Link>
-              </li>
-              <li>
-                <Link to="/signin">Log in</Link>
-              </li>
-            </ul>
-          )}
-        </div>
+        <Link to="/" className="navbar-brand">
+          <img src="" alt="Warbler Home" />
+        </Link>
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <Link to="/signup">Sign up</Link>
+          </li>
+          <li>
+            <Link to="/signin">Log in</Link>
+          </li>
+        </ul>
       </nav>
     );
   }
@@ -53,4 +30,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, { logout })(Navbar);
+export default connect(mapStateToProps)(Navbar);
