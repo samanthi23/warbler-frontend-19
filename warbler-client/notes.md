1d5f7e99388a33af4822c22c91d484198e690784
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-03-27 14:46:09 +0000

    mapped state to props navbar connected to redux store

---
+++ warbler-client/notes.md
@@ -0,0 +1,14 @@
+# our first container is called Navbar.js
+
+download Bootstrap CDN
+
+in /public/index.html just paste in CDN link
+
+Navbar connects to redux store if user authenticated or not Navbar is going to change
+
+# mapStateToProps
+
+needs current user for navbar
+
+
+
--- warbler-client/src/containers/Navbar.js
+++ warbler-client/src/containers/Navbar.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+// import from React and destructure Component from React
+
 import { Link } from "react-router-dom";
+// since this is a container we also want to connect it to our Redux store
 import { connect } from "react-redux";
 import { logout } from "../store/actions/auth";
 import Logo from "../images/warbler-logo.png";
@@ -8,9 +11,15 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand">
+      {/* return some JSX */ }
+      {/* start with nav element */}
+      {/* <div className="container-fluid"> */}
         <Link to="/" className="navbar-brand">
           <img src="" alt="Warbler Home" />
         </Link>
+        { /* </div> */}
+        
+        {/* navbar-right pushes everything to the right */}
         <ul className="nav navbar-nav navbar-right">
           <li>
             <Link to="/signup">Sign up</Link>
@@ -24,10 +33,19 @@ class Navbar extends Component {
   }
 }
 
+// need current User for Navbar
+
+
+// pass in state from redux store
 function mapStateToProps(state) {
   return {
+    // everything we get from the current user
+    
     currentUser: state.currentUser
   };
 }
 
+
+// no dispatch to props so just pass in null
+// wrap this again in our Navbar (mapStateToProps)(Navbar)
 export default connect(mapStateToProps)(Navbar);
